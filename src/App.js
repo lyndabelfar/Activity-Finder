fa9fb4d4b0c43e429587b33a7eaf2f601cd35b2c
lyndabelfar <lyndabelar@gmail.com>
2022-08-30 18:25:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState} from 'react';
+
+const url ="https://www.boredapi.com/api/activity/"
 
 function App() {
+  const [boringData, setBoringData] = useState([])
+  const [num, setNum] = useState(0)
+
+
+  const fetchData= async ()=>{
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    setBoringData(data)
+    
+
+  }
+  useEffect(()=>{
+    document.title ="The Bored Website"
+  }, [])
+
+  useEffect(()=>{
+    fetchData();
+  }, [num])
+
+  const increaseNum =()=>{
+    let newNum = num +1
+    setNum(newNum)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container '>
+      <h1>bored? do this</h1>
+      <p>"{boringData.activity}"</p>
+      <p>it's a {boringData.type} activity</p>
+
+      <div className="participant-num">Requires {boringData.participants} {boringData.participants===1?'participant': 'participants'}, {boringData.participants ===1 ?"no need to anyone :)":'yeah need some people with you, it may suck if you are an introvert...'}</div>
+      <button onClick={increaseNum} >Get another activity</button>
     </div>
   );
 }
